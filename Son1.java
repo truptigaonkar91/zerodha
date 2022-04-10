cfe429590cee2dd1191d5a5b4951c9801611904c
unknown <truptigaonkar91@gmail.com>
2022-04-10 16:02:17 +0530

    son1 added

---
+++ Son1.java
@@ -0,0 +1,13 @@
+package multilevel;
+
+public class Son1 extends Father1{
+
+	public void House() {
+		System.out.println("Grandfathers house");
+	}
+
+	public void Car() {
+		System.out.println("Grandfathers Car");
+	}
+
+}
